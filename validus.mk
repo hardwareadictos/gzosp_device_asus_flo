3d4662de94f9addf8d9d68bd40e77040b194cc3b
slebit <tillnagotannat@gmail.com>
2017-02-18 18:30:48 +0100

    Add maintainer

--- validus.mk
+++ validus.mk
@@ -36,3 +36,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
     BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
     PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Filip A(slebit)"
